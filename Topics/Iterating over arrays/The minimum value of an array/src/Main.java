6a7c88fa0e98503f2a51b1e111bbcbf2b02dc416
Rob <ae6sr@users.noreply.github.com>
2021-03-30 09:28:27 -0700

    Added Task

--- Topics/Iterating over arrays/The minimum value of an array/src/Main.java
+++ Topics/Iterating over arrays/The minimum value of an array/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+
+        // Input
+        Scanner scanner = new Scanner(System.in);
+        int arraySize = scanner.nextInt();
+        int[] numArray = new int[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            numArray[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+        // End input
+
+        // Processing and output
+        int minArray = numArray[0];
+        for (int i = 0; i < arraySize; i++) {
+            if (numArray[i] <= minArray) {
+                minArray = numArray[i];
+            }
+        }
+
+        System.out.println(minArray);
     }
 }
